45c757dcd07da0de66a52a88f5bfa365be7732d4
Tarek Bouamer <tarek.bouamer@tawazunti.ae>
2023-02-08 17:41:39 +0400

    fix colmap build

--- loc/tools/build_map_colmap.py
+++ loc/tools/build_map_colmap.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from loc.datasets.dataset import ImagesFromList
 from loc.mappers.colmap_mapper import ColmapMapper
 from loc.tools.convert import do_convert_3d_model
-from loc.tools.extraction import do_database_extraction
+from loc.tools.extraction import database_feature_extraction
 from loc.tools.matching import do_matching
 
 # logger
@@ -21,7 +21,7 @@ def build_map_colmap(cfg) -> None:
    
     # convert 3d model
     logger.info('convert 3D model to colmap format')
-    # do_convert_3d_model(cfg=cfg)
+    do_convert_3d_model(cfg=cfg)
 
     # mapper
     logger.info('init mapper')
@@ -29,37 +29,37 @@ def build_map_colmap(cfg) -> None:
 
     # covisibility
     logger.info('compute database covisibility pairs')
-    # sfm_pairs_path = mapper.covisible_pairs(sfm_pairs_path=sfm_pairs_path)
+    sfm_pairs_path = mapper.covisible_pairs(sfm_pairs_path=sfm_pairs_path)
 
     #
     logger.info('extract database features')
-    # db_features_path = do_database_extraction(workspace=cfg.workspace,
-    #                                                   save_path=cfg.visloc_path,
-    #                                                   cfg=cfg)
+    db_features_path = database_feature_extraction(workspace=cfg.workspace,
+                                                      save_path=cfg.visloc_path,
+                                                      cfg=cfg)
     
-    db_features_path  = Path(str(cfg.visloc_path) + '/' + str("db")    + '_local_features' + '.h5')
+    db_features_path  = Path(str(cfg.visloc_path) + '/' + str("db")    + '_features' + '.h5')
 
     logger.info('perform databse matching')
-    # sfm_matches_path = do_matching(src_path=db_features_path,
-    #                                dst_path=db_features_path,
-    #                                pairs_path=sfm_pairs_path,
-    #                                cfg=cfg,
-    #                                save_path=sfm_matches_path)
+    sfm_matches_path = do_matching(src_path=db_features_path,
+                                   dst_path=db_features_path,
+                                   pairs_path=sfm_pairs_path,
+                                   cfg=cfg,
+                                   save_path=sfm_matches_path)
     
     mapper.create_database()
 
     logger.info('import features to database')
-    # mapper.import_features(db_features_path)
+    mapper.import_features(db_features_path)
 
     logger.info('import matches to database')
-    # mapper.import_matches(sfm_pairs_path, sfm_matches_path)
+    mapper.import_matches(sfm_pairs_path, sfm_matches_path)
 
     logger.info('geometric verification')
-    # mapper.geometric_verification(
-    #     db_features_path, sfm_pairs_path, sfm_matches_path)
+    mapper.geometric_verification(
+        db_features_path, sfm_pairs_path, sfm_matches_path)
 
     # triangulate
     logger.info('triangulation')
-    # reconstruction = mapper.triangulate_points()
+    reconstruction = mapper.triangulate_points(verbose=True)
 
     return mapper, db_features_path
