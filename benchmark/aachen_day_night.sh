397670248c284b0f2faa3a16eb4c4f404c84c579
Tarek Bouamer <tarek.bouamer@tawazunti.ae>
2023-03-24 12:35:23 +0400

    aadchenDN benchmark

---
+++ benchmark/aachen_day_night.sh
@@ -0,0 +1,9 @@
+
+clear
+
+python3 setup.py install
+
+WORKSPACE='/media/dl/Data/datasets/aachen'
+WORKSPACE='/media/loc/D0AE6539AE65196C/VisualLocalization2020/aachen'
+
+aachen_day_night.py --workspace $WORKSPACE --config loc/configurations/default.yml
